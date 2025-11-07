dd66557d355f45ce6411c7c71929ca40f2c38f46
Jignas Paturu <86356085+JignasP@users.noreply.github.com>
2025-11-07 09:27:32 +0530

    Update CSV file path for data loading

--- covid_dataAnalytics.py
+++ covid_dataAnalytics.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-df = pd.read_csv("Set 4-countrywise_corona.csv")
+df = pd.read_csv("countrywise_corona.csv")
 
 print("Dataset Loaded", df.shape)
 df.head()
